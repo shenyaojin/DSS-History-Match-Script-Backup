f04b307fd8aec1334032d2e365423a55c259c9fe
shenyaojin <shenyaojin@mines.edu>
2025-11-08 18:18:26 -0700

    add new script

---
+++ DSS_history_match/108_misfit_func.py
@@ -0,0 +1,8 @@
+# This script will implement the discussion on misfit functions and other data processing techniques.
+# See my notion. -- Shenyao
+
+# - linear scaling for DSS data. Because DSS is not perfectly coupled with the casing. Let's do 10x
+# - remove one SRV (SRV2)
+# - change the misfit func, to a manually picked area.
+# - sensitivity  test on length
+
