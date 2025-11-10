fee455801e2cadac76ecd6f1526a6427cb1b80c0
shenyaojin <shenyaojin@mines.edu>
2025-11-10 14:34:05 -0700

    Add DSS area selection script and update misfit function setup

--- DSS_history_match/108_misfit_func.py
+++ DSS_history_match/108_misfit_func.py
@@ -6,3 +6,48 @@
 # - change the misfit func, to a manually picked area.
 # - sensitivity  test on length
 
+# This script, still will only use 1 fracture.
+
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from typing import List, Dict, Optional
+import datetime
+import csv
+
+from fiberis.analyzer.Data2D.Data2D_XT_DSS import DSS2D
+from fiberis.analyzer.Data1D.Data1D_Gauge import Data1DGauge
+from fiberis.analyzer.Data1D.Data1D_MOOSEps import Data1D_MOOSEps
+from fiberis.io.reader_moose_ps import MOOSEPointSamplerReader
+
+from fiberis.moose.model_builder import ModelBuilder
+from fiberis.moose.runner import MooseRunner
+from fiberis.moose.config import (
+    MatrixConfig, SRVConfig, HydraulicFractureConfig, ZoneMaterialProperties, SimpleFluidPropertiesConfig,
+    PointValueSamplerConfig, LineValueSamplerConfig, TimeSequenceStepper
+)
+
+# Optimization modules
+from skopt import gp_minimize
+from skopt.space import Real
+from skopt.utils import use_named_args
+
+#%% Set up paths
+# Source data paths
+DSS_datapath = "data/fiberis_format/s_well/dss_data/Mariner 14x-36-POW-S - RFS strain change.npz"
+pressure_gauge_g1_path = "data/fiberis_format/prod/gauges/pressure_g1.npz"
+injection_gauge_pressure_path = "data/fiberis_format/prod/gauges/gauge4_data_prod.npz"
+moose_executable = "/rcp/rcp42/home/shenyaojin/Documents/bakken_mariner/moose_env/moose/modules/porous_flow/porous_flow-opt"
+mpiexec_path = "/rcp/rcp42/home/shenyaojin/miniforge/envs/moose/bin/mpiexec"
+
+# Simulation and logging output paths
+project_name = "1109_DSS_SingleFrac_MisfitFunc"
+base_output_dir = os.path.join("output", project_name)
+moose_output_dir = os.path.join(base_output_dir, "moose_simulations")
+log_dir = os.path.join(base_output_dir, "optimization_logs")
+fig_parent_dir = os.path.join("figs", project_name)
+
+os.makedirs(moose_output_dir, exist_ok=True)
+os.makedirs(log_dir, exist_ok=True)
+os.makedirs(fig_parent_dir, exist_ok=True)
+
---
+++ DSS_history_match/108p_DSS_area_select.py
@@ -0,0 +1,58 @@
+# Before we do the history matching on 108,
+# Let's first select the area of interest using DSS data, so that I can design the misfit function coorrectly.
+# Shenyao Jin, shenyaojin@mines.edu, 11/09/2025
+
+#%% Import libs
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import datetime
+
+# I/O modules from fiberis
+from fiberis.analyzer.Data2D.Data2D_XT_DSS import DSS2D
+from fiberis.analyzer.Data1D.Data1D_Gauge import Data1DGauge
+
+# Define file paths
+DSS_datapath = "data/fiberis_format/s_well/dss_data/Mariner 14x-36-POW-S - RFS strain change.npz"
+pressure_gauge_g1_path = "data/fiberis_format/prod/gauges/pressure_g1.npz"
+
+#%% Load DSS data
+DSSdata = DSS2D()
+DSSdata.load_npz(DSS_datapath)
+
+# Load pressure gauge data
+pg_dataframe = Data1DGauge()
+pg_dataframe.load_npz(pressure_gauge_g1_path)
+
+#%% Pre=process DSS data
+mds = DSSdata.daxis
+ind = (mds > 7500) & (mds < 15000)
+drift_val = np.median(DSSdata.data[ind, :], axis=0)
+DSSdata.data -= drift_val.reshape((1, -1))
+DSSdata.select_time(0, 400000)
+DSSdata.select_depth(14820, 14920) # <- Select depth range of interest.
+# Might need to change based on well location
+
+#%% Pre-process pressure gauge data
+pg_dataframe.select_time(DSSdata.start_time, DSSdata.get_end_time())
+pg_dataframe.remove_abnormal_data(threshold=300, method='mean')
+
+#%% Co-plot DSS and pressure gauge data, use subplot2grid
+fig = plt.figure(figsize=(7, 6))
+ax1 = plt.subplot2grid((5, 4), (0, 0), rowspan=4, colspan=4) # <- DSS plot
+ax2 = plt.subplot2grid((5, 4), (4, 0), rowspan=1, colspan=4, sharex=ax1) # <- Pressure gauge plot
+
+# Plot DSS data
+im1 = DSSdata.plot(ax=ax1, use_timestamp=False, cmap='bwr', vmin=-1, vmax=1)
+ax1.set_title("DSS Data at POW-S")
+ax1.set_ylabel("Depth (ft)")
+clim = np.array([-1, 1])
+
+# Hide x-axis ticks
+ax1.tick_params(labelbottom=False)
+# Plot pressure gauge data
+im2 = pg_dataframe.plot(ax=ax2, use_timestamp=False)
+ax2.set_ylabel("Pressure (psi)")
+
+plt.tight_layout()
+plt.show()
